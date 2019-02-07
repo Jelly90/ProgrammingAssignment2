e81e03c7dab26019e9a8942bc7a6f88f210c8291
JellyMeerkat <j.strenger@gmx.net>
2019-02-07 22:36:29 +0100

    Added functions that are requested for the assignment as well as some lines to test the function.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,51 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The two functions presented here provide the possibility to 
+## creates a special "matrix" object that can cache its inverse.
+## Matrix inversion is usually a costly computation and there 
+##may be some benefit to caching the inverse of a matrix rather 
+## than compute it repeatedly
 
-## Write a short comment describing this function
+## This function creates a special "matrix" object that can 
+## cache its inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(inverse) m <<- inverse
+  getinverse <- function() m
+  list(set = set, get = get, 
+       setinverse = setinverse, 
+       getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## This function computes the inverse of the special "matrix" 
+## returned by makeCacheMatrix above. If the inverse has already 
+## been calculated (and the matrix has not changed), then the 
+## cachesolve should retrieve the inverse from the cache.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  m <- x$getinverse()
+  if(!is.null(m)){
+    message("getting chached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setinverse(m)
+  m
 }
+
+## Test functions:
+## test:
+
+## x <- c(3,4,5,6,7,8,5,7,1)
+## dim(x) <- c(3,3)
+
+## y <- cacheSolve(makeCacheMatrix(x))
+## y
+## round(x%*%y,10)
